7cec18f7a847d793bc287f918496549ac30dcdbe
JP Smith <jp.smith@ilstu.edu>
2013-10-11 21:34:58 -0500

    Mislabeled 2 problems

--- PE_18.py
+++ PE_18.py
@@ -1,39 +1,18 @@
 #!/usr/bin/env python
 
-units = ["", "one", "two", "three", "four",  "five", "six", "seven", "eight", "nine"]
-teens = ["", "eleven", "twelve", "thirteen",  "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-tens = ["", "ten", "twenty", "thirty", "forty","fifty", "sixty", "seventy", "eighty", "ninety"]
+triangle = [[75], [95, 64], [17, 47, 82], [18, 35, 87, 10], [20, 04, 82, 47, 65], [19, 01, 23, 75, 03, 34], [88, 02, 77, 73, 07, 63, 67], [99, 65, 04, 28, 06, 16, 70, 92], [41, 41, 26, 56, 83, 40, 80, 70, 33], [41, 48, 72, 33, 47, 32, 37, 16, 94, 29], [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14], [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57], [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48], [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31], [04, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 04, 23]]
 
-def word_length(n):
-    
-    answer = 0
-    
-    char_array = []
-    for i in str(n):
-        char_array.append(int(i))
-    
-    if len(char_array) == 1:
-        answer += len(units[n])
-    
-    else:
-        if char_array[-2] == 1 and char_array[-1] != 0:
-            answer += len(teens[char_array[-1]])
+for row_number, row in enumerate(triangle):
+  if row_number != 0:
+    for item_number, item in enumerate(row):
+      if item_number == 0:
+        row[item_number] = item + triangle[row_number - 1][item_number]
+      elif item_number + 1 == len(row):
+        row[item_number] = item + triangle[row_number - 1][item_number - 1]
+      else:
+        if triangle[row_number - 1][item_number - 1] > triangle[row_number - 1][item_number]:
+          row[item_number] = item + triangle[row_number - 1][item_number - 1]
         else:
-            answer += len(tens[char_array[-2]])
-            answer += len(units[char_array[-1]])
-    
-    if len(char_array) > 2:
-        answer += len(units[char_array[0]])
-        answer += len('hundred')
-        if char_array[1] != 0 or char_array[2] != 0:
-            answer += len('and')
-            
-    return answer
+          row[item_number] = item + triangle[row_number - 1][item_number]
 
-
-final_answer = 0
-for i in range(1, 1000):
-    final_answer += word_length(i)
-final_answer += len("onethousand")
-
-print final_answer
+print max(triangle[-1])
